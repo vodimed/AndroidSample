2bb532e0bc5ab24b3e476919d8db48e38a3c0926
vodimed <dem@dxdy.ru>
2019-07-31 21:46:32 +0300

    working

--- app/src/main/java/com/smartoperator/myapplication/MainActivity.java
+++ app/src/main/java/com/smartoperator/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.smartoperator.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     int i = 0;
@@ -11,7 +12,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.scroller).setOnClickListener(onClick);
+        TextView control = findViewById(R.id.scroller);
+        control.setOnClickListener(onClick);
+        control.setMovementMethod(new android.text.method.ScrollingMovementMethod());
     }
 
     View.OnClickListener onClick = new View.OnClickListener() {
